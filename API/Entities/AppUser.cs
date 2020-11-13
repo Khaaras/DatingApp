8c42ce31a8953559da39808bfcef9f5889f9b425
Lucas Lej <sacul69@gmail.com>
2020-11-13 18:46:15 +0100

    End Section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,8 +4,10 @@ namespace API.Entities
     {
         public int Id { get; set; }     // musi byc publiczne inaczej robiąć migrację nie zostałoby rozpoznane jako PrimaryKey
         public string UserName { get; set; }
+ 
+        public byte[] PasswordHash { get; set; }
 
-        
+        public byte[] PasswordSalt { get; set; }  
 
     }
 }
