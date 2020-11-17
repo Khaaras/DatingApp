d591b39bf48a96a9400e48ae187ebabb43e064a7
Lucas Lej <sacul69@gmail.com>
2020-11-17 13:50:31 +0100

    Section 7 Bug

--- API/Startup.cs
+++ API/Startup.cs
@@ -7,6 +7,7 @@ using API.Data;
 using API.Extensions;
 using API.Extensions;
 using API.Interfaces;
+using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
@@ -46,11 +47,12 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+            app.UseMiddleware<ExceptionMiddleware>();
+            // if (env.IsDevelopment())
+            // {
+            //     app.UseDeveloperExceptionPage();
                 
-            }
+            // }
 
             app.UseHttpsRedirection();
 
